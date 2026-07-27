ae37a1d42d23ff542733d326ea8e87225e9d0d1c
ahmxdkvl69 <ahmedalicust024@gmail.com>
2026-07-27 14:28:15 +0500

    Remove Dummy label from external inventory app

--- external_inventory_app.py
+++ external_inventory_app.py
@@ -37,7 +37,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>External Dummy Ticket Inventory Panel</title>
+    <title>External Ticket Inventory Panel</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;700&display=swap" rel="stylesheet">
